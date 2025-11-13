2a1044176be8d16b2016f600c3c1f1990eed444f
Vishwa thimira Vidarshana 1234 <thimira.vishwa2003@gmail.com>
2025-11-13 22:28:35 +0000

    see the data of firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,8 @@ export const firebaseConfig = {
   "appId": "1:339132750621:web:7cb74ec103ba25a6331ff4",
   "apiKey": "AIzaSyBSVM2kP0Pl-HY346icgc4jUPI0y7Pri2Q",
   "authDomain": "studio-8072496389-b03d4.firebaseapp.com",
-  "storageBucket": "studio-8072496389-b03d4.appspot.com",
+  "storageBucket": "studio-8072496389-b03d4.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "339132750621"
 };
+
